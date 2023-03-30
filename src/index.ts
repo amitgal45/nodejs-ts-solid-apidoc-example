48ff903ecc6ee709b2471b293d5fe6dbb55c562b
Amit gal <amitgal56@gmail.com>
2023-03-30 16:39:36 +0300

    dist

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,6 @@
 import AppFactory from "./config/app/AppFactory";
 
 const PORT = process.env.PORT || 3000;
-const MONGO_SRV = (process.env.MONGOSRV =
-  "mongodb://admin:password@localhost:27017/nest");
-
 const app = AppFactory.create();
 
 app.listen(PORT, () => {
