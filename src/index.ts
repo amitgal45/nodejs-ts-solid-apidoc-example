67efc9a9a85a65d66e0a6da73fc287373b11e6f9
Amit gal <amitgal56@gmail.com>
2023-04-03 14:49:09 +0300

    v 0.1 - fix v6

--- src/index.ts
+++ src/index.ts
@@ -7,8 +7,8 @@ const PORT = process.env.PORT || 3000;
 const app = AppFactory.create();
 
 const doc = createDoc({
-  src: path.resolve(__dirname, "/"),
-  dest: path.resolve(__dirname, "doc"), // can be omitted if dryRun is true
+  src: path.resolve(path.join(__dirname, '/')),
+  dest: path.resolve(path.join(__dirname, 'public')), // can be omitted if dryRun is true
   // if you don't want to generate the output files:
   dryRun: false,
   // if you don't want to see any log output:
@@ -20,7 +20,7 @@ if (typeof doc !== "boolean") {
   console.log(doc.data); // the parsed api documentation object
   console.log(doc.project); // the project information
 }
-app.use(express.static(path.join(__dirname, 'doc')))
+app.use(express.static(path.join(__dirname, 'public')))
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
