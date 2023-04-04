48fbb8030d48667478f50f122d98f5549a5364f4
Amit gal <amitgal56@gmail.com>
2023-04-04 12:54:23 +0300

    v0.12 - update italian league

--- src/routes/League.route.ts
+++ src/routes/League.route.ts
@@ -6,7 +6,6 @@ const router = Router();
 router.get("/", leagueController.findAll);
 router.get("/:id", leagueController.findById);
 router.get("/:id/team", leagueController.findAllTeamsById);
-
 router.post("/", leagueController.create);
 router.put("/:id", leagueController.update);
 router.delete("/:id", leagueController.delete);
